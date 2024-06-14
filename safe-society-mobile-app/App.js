83b15a010095385e15927a2295e8deb134555efd
Matthew Markovets <mmarkovec15072003@gmail.com>
2024-06-14 12:12:04 +0300

    Changes

--- safe-society-mobile-app/App.js
+++ safe-society-mobile-app/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hey th</Text>
+      <Text>Hey there</Text>
       <StatusBar style="auto" />
     </View>
   );
